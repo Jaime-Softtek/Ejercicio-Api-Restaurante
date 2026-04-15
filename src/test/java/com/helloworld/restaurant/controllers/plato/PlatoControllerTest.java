295dbf4cfa12f3a1cb6234370a6aee37ca599c96
Jaime Castillo <jaime.castillo@softtek.com>
2026-04-15 17:13:53 +0200

    Terminado PlatoDaoTest

--- src/test/java/com/helloworld/restaurant/controllers/plato/PlatoControllerTest.java
+++ src/test/java/com/helloworld/restaurant/controllers/plato/PlatoControllerTest.java
@@ -1,4 +1,11 @@
 package com.helloworld.restaurant.controllers.plato;
 
+import com.helloworld.restaurant.daos.plato.PlatoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+
 public class PlatoControllerTest {
+
 }
