75468e2cb3a533af47d2306b1b5a9477ab182d9b
SOFTTEKPROJECTS\fabiola.novoa <fabiola.novoa@softtek.com>
2026-04-15 16:48:28 +0200

    SOLO creacion de archivo test restaurante dao, service

---
+++ src/test/java/com/helloworld/restaurant/daos/restaurante/RestauranteDaoImplTest.java
@@ -0,0 +1,28 @@
+package com.helloworld.restaurant.daos.restaurante;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestauranteDaoImplTest {
+
+    @Test
+    void getRestaurantes() {
+    }
+
+    @Test
+    void getRestauranteByCif() {
+    }
+
+    @Test
+    void saveRestaurante() {
+    }
+
+    @Test
+    void eliminarRestaurante() {
+    }
+
+    @Test
+    void editRestaurante() {
+    }
+}
---
+++ src/test/java/com/helloworld/restaurant/services/restaurante/RestauranteServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.helloworld.restaurant.services.restaurante;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestauranteServiceImplTest {
+
+    @Test
+    void getAllRestaurantes() {
+    }
+
+    @Test
+    void getRestauranteByCif() {
+    }
+
+    @Test
+    void getCartaFromRestaurante() {
+    }
+
+    @Test
+    void createRestaurante() {
+    }
+
+    @Test
+    void deleteRestaurante() {
+    }
+
+    @Test
+    void modifyRestaurante() {
+    }
+}
