d3f9bafe9fdbb6751729624635bc254598577b91
SOFTTEKPROJECTS\daniel.moralesa <daniel.moralesa@softtek.com>
2026-04-17 10:47:17 +0200

    esto está potencialmente mal pero lo voy a necesitar luego de la clase de open api

--- src/test/java/com/helloworld/restaurant/daos/restaurante/RestauranteDaoImplTest.java
+++ src/test/java/com/helloworld/restaurant/daos/restaurante/RestauranteDaoImplTest.java
@@ -4,21 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 
 @SpringBootTest
-@Transactional
+@ActiveProfiles("test")
+@Sql("/schema.sql")
 class RestauranteDaoImplTest {
 
     @Autowired private RestauranteDao restauranteDao;
 
     @Test
+    @Sql(statements = {"INSERT INTO restaurante(cif, nombre, direccion, telefono) VALUES('A12345678', 'La Mar Salada', 'Calle Mayor, 1', '123456789');"})
     void ShouldReturnAllRestaurantes() throws Exception {
         var restaurante = restauranteDao.getRestaurantes();
 
-        Assertions.assertEquals(3, restaurante.size());
+        Assertions.assertEquals(1, restaurante.size());
         Assertions.assertEquals("A12345678", restaurante.get(0).cif());
     }
 
